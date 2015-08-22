0f3c26633b5128a53aa075738d30eaac513eb7aa
Doug Moscrop <doug.moscrop@gmail.com>
2015-08-22 00:13:46 -0400

    Typical shoulda-done-it-differently cleanup

--- index.js
+++ index.js
@@ -1,75 +1,87 @@
 var Bluebird = require('bluebird'),
     bluebirdEvents = require('bluebird-events'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    mongodbUri = require('mongodb-uri');
 
 var connections = {};
 
 function isConnected(connection) {
-    return connection.readyState === mongoose.STATES.connected;
+  return connection.readyState === mongoose.STATES.connected;
 }
 
-function connect(url, connectionOptions) {
-    var connection = connections[url];
+function connect(url, options) {
+  var connection = connections[url];
 
-    if (!connection) {
-        connection = connections[url] = mongoose.createConnection(url, connectionOptions);
+  if (!connection) {
+    connection = connections[url] = mongoose.createConnection(url, options);
 
-        connection.on('close', function() {
-            delete connections[url];
-        });
-
-        process.on('exit', function() {
-            if (isConnected(connection)) {
-                connection.close();
-            }
-        });
-    }
-    
-    return connection;
-}
-
-module.exports = function(config, done) {
-    config = config || {};
-    
-    if (typeof config === 'string') {
-        config = {
-            url: config
-        };
-    }
-    
-    var connectionOptions = config.options || { db: { w: 'majority' } };
-    var connectionTimeout = config.timeout || 5000;
-    
-    var connection = connect(config.url, connectionOptions);
-    
-    if (config.database) {
-        connection = connection.useDb(config.database);
-    }
-
-    Object.keys(mongoose.models).forEach(function(key) {
-        var model = mongoose.models[key];
-
-        connection.model(model.modelName, model.schema);
+    connection.on('close', function() {
+      delete connections[url];
     });
 
-    var promise;
-    
-    if (isConnected(connection)) {
-        promise = Bluebird.resolve(connection);
-    } else {
-        promise = bluebirdEvents(connection, {
-            resolve: 'open',
-            reject: 'error'
+    process.on('exit', function() {
+      if (isConnected(connection)) {
+        connection.close();
+      }
+    });
+  }
+
+  if (isConnected(connection)) {
+    return Bluebird.resolve(connection);
+  } else {
+    return bluebirdEvents(connection, {
+      resolve: 'open',
+      reject: 'error'
+    }).then(function() {
+      return connection;
+    });
+  }
+}
+
+function configure(opts) {
+  var config = opts || {};
+
+  if (typeof config === 'string') {
+    config = {
+      url: config
+    };
+  }
+
+  config.options = config.options || {db: {w: 'majority'}};
+  config.timeout = config.timeout || 5000;
+
+  var uri = mongodbUri.parse(config.url);
+
+  config.database = config.database || uri.database;
+
+  delete uri.database;
+
+  config.url = mongodbUri.format(uri);
+
+  return config;
+}
+
+module.exports = function(opts, done) {
+  var config = configure(opts);
+
+  return connect(config.url, config.options)
+        .then(function(connection) {
+          var db = connection.useDb(config.database);
+
+          Object.keys(mongoose.models).forEach(function(key) {
+            var model = mongoose.models[key];
+
+            db.model(model.modelName, model.schema);
+          });
+
+          return db;
         })
-        .then(function() {
-            return connection;
-        })
-        .timeout(connectionTimeout);
-    }
-    
-    if (typeof done === 'function') {
-        promise.nodeify(done);
-    } else {
-        return promise;
-    }
+        .timeout(config.timeout);
 };
+
+Object.defineProperty(module.exports, 'connections', {
+  enumerable: false,
+  configurable: false,
+  writable: false,
+  value: connections
+});
--- spec.js
+++ spec.js
@@ -2,74 +2,65 @@ var assert = require('assert'),
     mongoose = require('mongoose'),
     mobgoose = require('./');
 
-var model = mongoose.model('Foo', new mongoose.Schema({}));
+var Foo = mongoose.model('Foo', new mongoose.Schema({}));
 
-it('can works via callback', function(done) {
-    mobgoose({
-        url: 'mongodb://localhost:27017',
-        database: 'test'
-    }, done);
-});
-
-it('works via promise', function() {
-    return mobgoose({
-        url: 'mongodb://localhost:27017',
-        database: 'test'
-    });
-});
-
-it('supports a direct conection string', function() {
-    return mobgoose('mongodb://localhost:27017/test')
+it('supports a simple conection string', function() {
+  return mobgoose('mongodb://localhost:27017/test')
         .then(function(connection) {
-            var model = connection.model('Foo');
-        
-            assert(model.db.name === 'test');
+          var model = connection.model('Foo');
+
+          assert(model.db.name === 'test');
         });
 });
 
-describe('connecting to two different databases on the same server', function(done) {
-    var connection1, connection2;
-    
-    before(function(done) {
-        mobgoose({
-            url: 'mongodb://localhost:27017',
-            database: 'test1'
-        }, function(err, connection) {
-            if (err) {
-                done(err);
-            } else {
-                connection1 = connection;
-                done();
-            }
-        });
-    });
-    
-    before(function(done) {
-        mobgoose({
-            url: 'mongodb://localhost:27017',
-            database: 'test2'
-        }, function(err, connection) {
-            if (err) {
-                done(err);
-            } else {
-                connection2 = connection;
-                done();
-            }
-        });
-    });
-    
-    it('produces a connection in the correct readyState', function() {
-        assert(connection1.readyState === mongoose.STATES.connected);
-        assert(connection2.readyState === mongoose.STATES.connected);
-    });
-    
-    it('registers a model defined before the connection was created (globally)', function() {
-        assert(connection1.model('Foo') !== undefined);
-        assert(connection1.model('Foo').modelName === 'Foo');
-        assert(connection1.model('Foo').db.name === 'test1');
+it('can override database name of connection string', function() {
+  return mobgoose({
+    url: 'mongodb://localhost:27017/test',
+    database: 'test2'
+  }).then(function(connection) {
+    var model = connection.model('Foo');
 
-        assert(connection2.model('Foo') !== undefined);
-        assert(connection2.model('Foo').modelName === 'Foo');
-        assert(connection2.model('Foo').db.name === 'test2');
+    assert(model.db.name === 'test2');
+  });
+});
+
+describe('two different databases on the same server', function(done) {
+  var connection1, connection2;
+
+  before(function() {
+    return mobgoose({
+      url: 'mongodb://localhost:27017',
+      database: 'test1'
+    }).then(function(connection) {
+      connection1 = connection;
     });
+  });
+
+  before(function() {
+    return mobgoose({
+      url: 'mongodb://localhost:27017',
+      database: 'test2'
+    }).then(function(connection) {
+      connection2 = connection;
+    });
+  });
+
+  it('use one actual connection', function() {
+    assert(Object.keys(mobgoose.connections).length === 1);
+  });
+
+  it('produce connections in the connected readyState', function() {
+    assert(connection1.readyState === mongoose.STATES.connected);
+    assert(connection2.readyState === mongoose.STATES.connected);
+  });
+
+  it('register their own models', function() {
+    assert(connection1.model('Foo') !== undefined);
+    assert(connection1.model('Foo').modelName === Foo.modelName);
+    assert(connection1.model('Foo').db.name === 'test1');
+
+    assert(connection2.model('Foo') !== undefined);
+    assert(connection2.model('Foo').modelName === Foo.modelName);
+    assert(connection2.model('Foo').db.name === 'test2');
+  });
 });
